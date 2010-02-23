1e20a6f0cb1c3ebea0508d6a065cffd7a7816bc2
Alexander Stockinger <astockinger@crealytics.de>
2010-02-23 09:58:35 +0100

    POM update and removed obsolete class

--- src/main/java/org/restlet/configuration/modules/GwtRestletModule.java
+++ src/main/java/org/restlet/configuration/modules/GwtRestletModule.java
@@ -3,19 +3,16 @@ package org.restlet.configuration.modules;
 import java.util.Arrays;
 import java.util.List;
 
-import org.restlet.Client;
 import org.restlet.Context;
 import org.restlet.engine.ClientHelper;
 import org.restlet.engine.Helper;
 import org.restlet.engine.http.GwtHttpClientHelper;
 
 import com.google.gwt.inject.client.AbstractGinModule;
-import com.google.gwt.inject.rebind.adapter.GinModuleAdapter;
-import com.google.inject.Guice;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
+ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.TypeLiteral;
+
 
 public class GwtRestletModule extends AbstractGinModule {
 
@@ -37,9 +34,5 @@ public class GwtRestletModule extends AbstractGinModule {
 			return Arrays.asList((ClientHelper) helper);
 		}
 	}	
-	public static void main(String[] args) {
-		Injector injector = Guice.createInjector(new GinModuleAdapter(new RestletModule()));
-		Client client = injector.getInstance(Client.class);
-		System.out.println(client);
-	}
+	
 }
